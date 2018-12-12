9af1c7b18911b42aa45e3a227b1a2df82befc15b
Alexandra DeWit <alexa.dewit@gmail.com>
2018-12-12 12:00:40 +0100

    Cleanup

--- src/Data/ArrayBuffer/Encoding.purs
+++ src/Data/ArrayBuffer/Encoding.purs
@@ -11,16 +11,17 @@ import Data.TextDecoder (decodeUtf8)
 import Data.TextEncoder (encodeUtf8)
 import Data.ArrayBuffer.Typed (asUint8Array, dataView)
 import Data.ArrayBuffer.DataView (buffer, whole)
+
 -- | Convert a UTF-8 encoded `ArrayBuffer` into a `String`.
 -- | Serves as a quick utility function for a common use-case. For more use-cases,
--- | see: [purescript-text-encoding](https://pursuit.purescript.org/packages/purescript-text-encoding/0.0.8)
+-- | see: [purescript-text-encoding](https://pursuit.purescript.org/packages/purescript-text-encoding/0.0.9)
 -- | Requires the TextDecoder class available. A polyfill can be found in the npm package "text-encoding"
 decodeToString :: ArrayBuffer -> Either Error String
 decodeToString = decodeUtf8 <<< asUint8Array <<< whole
 
 -- | Convert a  `String` into a UTF-8 encoded `ArrayBuffer`.
 -- | Serves as a quick utility function for a common use-case. For more use-cases,
--- | see: [purescript-text-encoding](https://pursuit.purescript.org/packages/purescript-text-encoding/0.0.8)
+-- | see: [purescript-text-encoding](https://pursuit.purescript.org/packages/purescript-text-encoding/0.0.9)
 -- | Requires the TextDecoder class available. A polyfill can be found in the npm package "text-encoding"
 fromString :: String -> ArrayBuffer
 fromString = buffer <<< dataView <<< encodeUtf8
