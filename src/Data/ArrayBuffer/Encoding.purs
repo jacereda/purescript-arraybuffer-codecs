af8462720df6b08a413ba33a589e1268a78ba5a4
Alexandra DeWit <alexa.dewit@gmail.com>
2018-12-12 12:18:40 +0100

    Add base64 dependency and re-exports

--- src/Data/ArrayBuffer/Encoding.purs
+++ src/Data/ArrayBuffer/Encoding.purs
@@ -1,16 +1,18 @@
 module Data.ArrayBuffer.Encoding
-  (decodeToString
+  ( decodeToString
   , fromString
+  , module Base64
   ) where
 
 import Prelude ((<<<))
-import Data.Either (Either)
-import Effect.Exception (Error)
 import Data.ArrayBuffer.Types (ArrayBuffer)
-import Data.TextDecoder (decodeUtf8)
-import Data.TextEncoder (encodeUtf8)
 import Data.ArrayBuffer.Typed (asUint8Array, dataView)
 import Data.ArrayBuffer.DataView (buffer, whole)
+import Data.Base64(encodeBase64, decodeBase64) as Base64
+import Data.Either (Either)
+import Data.TextDecoder (decodeUtf8)
+import Data.TextEncoder (encodeUtf8)
+import Effect.Exception (Error)
 
 -- | Convert a UTF-8 encoded `ArrayBuffer` into a `String`.
 -- | Serves as a quick utility function for a common use-case. For more use-cases,
